cbc8baed23b8dcb63ebfa68c397236a3ec09c92f
gniofreitas <gniofreitas@gmail.com>
2018-07-13 17:58:24 -0400

    Add files via upload

---
+++ artifactid/src/main/java/groupid/artifactid/MyApplication.java
@@ -0,0 +1,24 @@
+package groupid.artifactid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication implements CommandLineRunner {
+ 
+    @Autowired
+    private YAMLConfig myConfig;
+ 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MyApplication.class);
+        app.run();
+    }
+ 
+    public void run(String... args) throws Exception {
+        System.out.println("using environment: " + myConfig.getEnvironment());
+        System.out.println("name: " + myConfig.getName());
+        System.out.println("servers: " + myConfig.getServers());
+    }
+}
